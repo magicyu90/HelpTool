f7e8e71aad84b31520d788ee6f758428689ff366
magicyu90 <magicyu90@gmail.com>
2017-11-14 13:32:03 +0800

    refactor ToolHelperV3.py

--- ToolHelperV3.py
+++ ToolHelperV3.py
@@ -1,22 +1,43 @@
-#coding=utf-8
+# coding=utf-8
 import codecs
 import xml.etree.ElementTree as ET
+import os
+import shutil
 
-#对于符合规范的xml文件直接，进行反序列化操作
-tree = ET.parse('ManualLabel.xml')
-root = tree.getroot()
-susCount = 0
-for sus in root.findall('sus'):
-    susCount += 1
+srcDir = "E:\\zx"
+destDir = "E:\\zxcopy"
+dirs = os.listdir(srcDir)
+
+# 对于符合规范的xml文件直接，进行反序列化操作
+# tree = ET.parse('ManualLabel.xml')
+# root = tree.getroot()
+# susCount = 0
+# for sus in root.findall('sus'):
+#     susCount += 1
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    '''Copy src directory to dest directory'''
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
 
 
-#对于不符合规范的xml文件，读取xml文件中的字符串
-xmlFile = open('ManualLabel2.xml', 'r', encoding="utf8")
-xmlFileLines = xmlFile.readlines()[2:]
-xmlFileStr = ''.join(xmlFileLines)
-root2 = ET.fromstring(xmlFileStr)
-
-susCount2 = 0
-for sus in root2.findall('sus'):
-    susCount2 += 1
-print('susCount for ManualLabel2.xml:', susCount2)
+for currentDir in dirs:
+    dirToEnter = srcDir + "\\" + currentDir + "\\" + "ManualLabel"
+    manualLabelFiles = os.listdir(dirToEnter)
+    for file in manualLabelFiles:
+        if file.endswith('.xml'):
+            susCount = 0
+            xmlFile = open(dirToEnter + "\\" + file, 'r', encoding="utf8")
+            xmlFileLines = xmlFile.readlines()[2:]
+            xmlFileStr = ''.join(xmlFileLines)
+            root = ET.fromstring(xmlFileStr)
+            for sus in root.findall('sus'):
+                susCount += 1
+            if susCount == 1:
+                print('copy dir:', srcDir + "\\" + currentDir)
+                copytree(srcDir + "\\" + currentDir, destDir + "\\" + currentDir)
