dbe5ddb23ac6105765b832aa3a0d9c0dd967c321
magicyu90 <magicyu90@gmail.com>
2017-11-14 12:51:01 +0800

    optimize app

--- ToolHelperV3.py
+++ ToolHelperV3.py
@@ -2,16 +2,15 @@
 import codecs
 import xml.etree.ElementTree as ET
 
+#对于符合规范的xml文件直接，进行反序列化操作
 tree = ET.parse('ManualLabel.xml')
 root = tree.getroot()
-
-print('root:', root)
 susCount = 0
 for sus in root.findall('sus'):
     susCount += 1
 
-print('susCount for ManualLabel.xml:', susCount)
 
+#对于不符合规范的xml文件，读取xml文件中的字符串
 xmlFile = open('ManualLabel2.xml', 'r', encoding="utf8")
 xmlFileLines = xmlFile.readlines()[2:]
 xmlFileStr = ''.join(xmlFileLines)
@@ -20,5 +19,4 @@ root2 = ET.fromstring(xmlFileStr)
 susCount2 = 0
 for sus in root2.findall('sus'):
     susCount2 += 1
-
 print('susCount for ManualLabel2.xml:', susCount2)
