f290e331c1874947ed305d74836d2c3f517dc723
magicyu90 <magicyu90@gmail.com>
2017-12-13 19:24:15 +0800

    update tool

---
+++ ToolForCreateSameFolder.py
@@ -0,0 +1,35 @@
+# coding:utf-8
+import os
+import shutil
+
+# currentpath = os.getcwd()
+currentpath = "C:\\Users\\shenyu1.NUCTECH\Desktop\\test"
+
+# for file in os.listdir(currentpath):
+#     filename = os.path.splitext(file)[0]
+#     extension = os.path.splitext(file)[1]
+#     filepath = os.path.join(currentpath, file)
+#     if extension == '.img':
+#         print(file)
+#         newdirpath = os.path.join(currentpath, filename)
+#         if not os.path.exists(newdirpath):
+#             os.makedirs(newdirpath)
+#             shutil.move(filepath, newdirpath)
+
+
+def move_image(path):
+    for file in os.listdir(path):
+        filename = os.path.splitext(file)[0]
+        extension = os.path.splitext(file)[1]
+        if os.path.isdir(os.path.join(path, file)):
+            move_image(os.path.join(path, file))
+        else:
+            if extension == ".img":
+                newdirpath = os.path.join(path, filename)
+                if not os.path.exists(newdirpath):
+                    os.makedirs(newdirpath)
+                    shutil.move(os.path.join(path, file), newdirpath)
+
+
+if __name__ == '__main__':
+    move_image(currentpath)
